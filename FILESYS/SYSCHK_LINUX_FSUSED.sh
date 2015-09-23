72823a66364925177e42dbdecaa545c4b89b43b9
ycl <yangchunlai@idcos.com>
2015-09-23 16:03:06 +0800

    linux health check

---
+++ FILESYS/SYSCHK_LINUX_FSUSED.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+#************************************************#
+# 文件名：SYSCHK_LINUX_FSUSED             #
+# 作  者：ycl                                   #
+# 日  期：2015年 12月25日                        #
+# 功  能：检查文件系统利用率                     #
+# 复核人：                                       #
+#************************************************#
+
+#检查临时脚本输出目录是否存在
+export LANG=en_US.utf8
+
+#mount point和相关使用率百分比大小
+df -P|grep -ivE "Filesystem"|awk '{print "Filesystem:"$1,"Total:"$2,"Used:"$3,"Capacity:"$5}'
+exit $?
